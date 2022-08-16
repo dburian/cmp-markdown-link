260165c785f0ab4858e0e3bafc702c343b025e82
David Burian <david.burian@me.com>
2022-08-17 00:49:28 +0200

    Added support for inline and wiki links.

--- lua/cmp-markdown-link/init.lua
+++ lua/cmp-markdown-link/init.lua
@@ -5,10 +5,13 @@ local Path = require('plenary.path')
 local default_option = {
   reference_link_location = 'bottom',
   searched_depth = 5,
+  style = 'reference', -- possible: 'reference', 'wiki', 'inline'
+  wiki_base_url = '',
+  wiki_end_url = '',
 }
 
 -- Helper functions
-local function get_notes(opt)
+local function load_all_targets(opt)
   local scanned_path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':h')
   local paths = scandir.scan_dir(scanned_path, {
     add_dirs = false,
@@ -32,6 +35,10 @@ local function get_notes(opt)
 end
 
 local function get_buf_links(buffer)
+  if not buffer then
+    buffer = 0
+  end
+
   local buf_lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
   local lines = {}
   for _, line in ipairs(buf_lines) do
@@ -44,44 +51,37 @@ local function get_buf_links(buffer)
   return lines
 end
 
--- nvim-cmp source
-local source = {}
-
-function source.new()
-  local self = setmetatable({}, {__index = source})
-
-  return self
-end
-
-function source:get_debug_name()
-  -- TODO: Add links to headings
-  return 'markdown link to files completion'
-end
-
-function source:is_available()
-  return vim.opt.filetype:get() == 'markdown'
-end
-
-function source.get_trigger_characters()
-  return {'['}
-end
-
-function source:complete(params, callback)
-  -- TODO: Add support for inline links
-  -- Only display entries for link targets
-  if not vim.endswith(params.context.cursor_before_line, '][') then
-    return callback()
+local function sanitize_opts(opts)
+  if opts.style ~= 'reference' and
+    opts.style ~= 'wiki' and
+    opts.style ~= 'inline' then
+    opts.style = nil
   end
 
-  local opt = vim.tbl_extend('keep', params.option, default_option)
-  local linked_notes = get_buf_links(0)
-  local notes = get_notes(opt)
+  local sanitized = vim.tbl_extend('keep', opts, default_option)
+
+  return sanitized
+end
+
+local function place_for_link(opts, context)
+  local ending = ']['
+  if opts.style == 'inline' then
+    ending = ']('
+  elseif opts.style == 'wiki' then
+    ending = '[['
+  end
+
+  return vim.endswith(context.cursor_before_line, ending)
+end
+
+local function create_ref_link_entries(targets, opts)
+  local linked_notes = get_buf_links()
 
   local line_count = vim.api.nvim_buf_line_count(0)
-  local ref_link_loc = opt.reference_link_location == 'top' and 0 or line_count
+  local ref_link_loc = opts.reference_link_location == 'top' and 0 or line_count
 
   local entries = {}
-  for _, note in ipairs(notes) do
+  for _, note in ipairs(targets) do
     local link_ref = '[' .. note.id .. ']: ' .. note.rel_path
 
     note.id = linked_notes[note.rel_path] or note.id
@@ -121,6 +121,100 @@ function source:complete(params, callback)
 
     table.insert(entries, entry)
   end
+
+  return entries
+end
+
+local function create_inline_link_entries(targets, _)
+  local entries = {}
+  for _, note in ipairs(targets) do
+
+    local entry = {
+      label = note.rel_path,
+      kind = cmp.lsp.CompletionItemKind.File,
+      documentation = {
+        kind = 'markdown',
+        value = note.contents
+      },
+      insertText = note.rel_path .. ')'
+    }
+
+    table.insert(entries, entry)
+  end
+
+  return entries
+end
+
+local function create_wiki_link_entries(targets, opts)
+  local entries = {}
+  for _, note in ipairs(targets) do
+    local link_label = note.rel_path
+
+    if #opts.wiki_base_url > 0 and
+      vim.startswith(link_label, opts.wiki_base_url) then
+      link_label = string.sub(link_label, #opts.wiki_base_url + 1)
+    end
+
+    if #opts.wiki_end_url > 0 and
+      vim.endswith(link_label, opts.wiki_end_url) then
+      link_label = string.sub(link_label, 0, -#opts.wiki_end_url - 1)
+    end
+
+    local entry = {
+      label = note.rel_path,
+      kind = cmp.lsp.CompletionItemKind.File,
+      documentation = {
+        kind = 'markdown',
+        value = note.contents
+      },
+      insertText = link_label .. ']]'
+    }
+
+    table.insert(entries, entry)
+  end
+
+  return entries
+end
+
+local create_entries = {
+  reference = create_ref_link_entries,
+  inline = create_inline_link_entries,
+  wiki = create_wiki_link_entries,
+}
+
+-- nvim-cmp source
+local source = {}
+
+function source.new()
+  local self = setmetatable({}, {__index = source})
+
+  return self
+end
+
+function source:get_debug_name()
+  -- TODO: Add links to headings
+  return 'markdown link to files completion'
+end
+
+function source:is_available()
+  return vim.opt.filetype:get() == 'markdown'
+end
+
+function source.get_trigger_characters()
+  return {'[', '('}
+end
+
+function source:complete(params, callback)
+  local opts = sanitize_opts(params.option)
+
+  -- Only display entries for link targets
+  if not place_for_link(opts, params.context) then
+    return callback()
+  end
+
+  local targets = load_all_targets(opts)
+
+  local entries = create_entries[opts.style](targets, opts)
 
   callback(entries)
 end
