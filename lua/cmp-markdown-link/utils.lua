d24202706a74457333a125ed010da04dbb242565
David Burian <david.burian@me.com>
2022-10-03 08:01:58 +0200

    Added option for searching additional dirs.

--- lua/cmp-markdown-link/utils.lua
+++ lua/cmp-markdown-link/utils.lua
@@ -3,17 +3,35 @@ local Path = require('plenary.path')
 
 local M = {}
 
-
 function M.load_all_targets(opts)
-  local paths = scandir.scan_dir(opts.cwd, {
+  local scan_opts = {
     add_dirs = false,
     hidden = false,
     depth = opts.searched_depth,
+    -- TODO: Add option for searching custom dirs
     search_pattern = '.*%.md',
-  })
+  }
+
+  local scan_dirs = opts.searched_dirs or {}
+  table.insert(scan_dirs, opts.cwd)
+  scan_dirs = vim.tbl_map(vim.fn.expand, scan_dirs)
+  scan_dirs = vim.fn.sort(scan_dirs)
+  scan_dirs = vim.fn.uniq(scan_dirs)
+
+  local paths = {}
+  for _, dir in ipairs(scan_dirs) do
+    table.insert(paths, scandir.scan_dir(dir, scan_opts))
+  end
+
+  paths = vim.tbl_flatten(paths)
+  -- In case some scan_dir was parent of another one and searched_depth was
+  -- large enough.
+  paths = vim.fn.sort(paths)
+  paths = vim.fn.uniq(paths)
 
   local notes = {}
   for _, path in ipairs(paths) do
+    -- TODO: It is not relative really...
     local rel_path = Path.new(path):make_relative(opts.cwd)
     table.insert(notes, {
       path = path,
